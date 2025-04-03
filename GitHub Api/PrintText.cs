fad4612f6fc1b4d106b3b5ec17e964c69df0d035
Arash <arash.mahdavi80@gmail.com>
2025-04-03 19:01:28 +0330

    Add project files.

---
+++ GitHub Api/PrintText.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub_Api;
+
+public static class PrintText
+{
+    public static void Simple(string text)
+    {
+        Console.WriteLine($"{text}");
+    }
+
+    public static void Green(string text)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"{text}");
+        Console.ResetColor();
+    }
+
+    public static void Yellow(string text)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine($"{text}");
+        Console.ResetColor();
+    }
+
+    public static void Blue(string text)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine($"{text}");
+        Console.ResetColor();
+    }
+
+    public static void DarkBlue(string text)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        Console.WriteLine($"{text}");
+        Console.ResetColor();
+    }
+
+    public static void Red(string text)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"{text}");
+        Console.ResetColor();
+    }
+}
---
+++ GitHub Api/Program.cs
@@ -0,0 +1,59 @@
+﻿using GitHub_Api;
+using System.Net.Http.Json;
+
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        PrintText.Green("Enter Username:");
+        string username = Console.ReadLine();
+        string apiUrl = $"https://api.github.com/users/{username}/events";
+
+        HttpClient client = new HttpClient();
+        client.DefaultRequestHeaders.Add("User-Agent", "C# HttpClient");
+        try
+        {
+            var events = await client.GetFromJsonAsync<GitHubEvent[]>(apiUrl);
+            bool isNull = events.Any();
+            if (isNull)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine($"\nLast events for {username}:");
+                Console.ResetColor();
+                int i = 1;
+                foreach (var ev in events)
+                {
+                    Console.WriteLine($"{i}- Type: {ev.type}, Repo: {ev.repo?.name}, Created at: {ev.created_at}");
+                    i = i + 1;
+                }
+            }
+            else
+            {
+                PrintText.Red("There Was No Record For The Current Username.");
+            }
+        }
+        catch (HttpRequestException ex)
+        {
+            string statusCode = (ex.StatusCode).ToString();
+
+            if (statusCode == "NotFound")
+                PrintText.Red("Error: User Not Found");
+
+            else
+                throw ex;
+        }
+    }
+}
+
+public class GitHubEvent
+{
+    public string type { get; set; }
+    public DateTime created_at { get; set; }
+    public Repo repo { get; set; }
+}
+
+public class Repo
+{
+    public string name { get; set; }
+}
